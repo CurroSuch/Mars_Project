557aa3a9b7dd9ac0b81845207ed01a9f61b5dcc1
Francisco Miguel Such Llinás <68127766+CurroSuch@users.noreply.github.com>
2022-02-04 11:27:51 +0100

    Update index.js

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,14 +1,15 @@
-const fetch = require ('node-fetch')
-const express = require ('express')
-const bodyParser = require ('body-parser')
-const path = require ('path')
+import fetch from 'node-fetch'
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import dotenv from 'dotenv'
 
-require('dotenv').config()
+dotenv.config()
 
 const app = express()
 const port = 3000
 
-app.use(express.static('./src/public'))
+app.use(express.static('./dist'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -19,30 +20,31 @@ app.get('/apod/:rover', async (req, res) => {
         let i = 0;
         let response;
         let images = { photos: [] }; 
-        while (images.photos.length == 0) {
+        while (Array.isArray(images.photos) && images.photos.length == 0) {
             const today = new Date();
             today.setDate(today.getDate()-i)
             let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate());
             if (rover ==='Curiosity'){
                 date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate());
-                let URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
-                response = await fetch(URL);
-                images = await response.json();
                 i++;
-            } else if (rover === 'Opportunity'){
-                date = today.getFullYear()-4+'-'+(today.getMonth()+6)+'-'+(today.getDate())
                 let URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
                 console.log(URL)
                 response = await fetch(URL);
                 images = await response.json();
+            } else if (rover === 'Opportunity'){
+                date = today.getFullYear()-4+'-'+(today.getMonth()+5)+'-'+(today.getDate())
                 i++;
+                let URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
+                console.log(URL)
+                response = await fetch(URL);
+                images = await response.json();
             } else if (rover === 'Spirit'){
                 date = today.getFullYear()-12+'-'+(today.getMonth()+3)+'-'+(today.getDate())
+                i++;
                 let URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
                 console.log(URL)
                 response = await fetch(URL);
                 images = await response.json();
-                i++;
             }
         }
         res.send({ images })
@@ -52,7 +54,3 @@ app.get('/apod/:rover', async (req, res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-
